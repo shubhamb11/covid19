b3e27831688791a5211f2fefaae9ab8dce7cfff5
Shubham Bhardwaj <Shubham@chalopadho.com>
2020-05-13 03:56:50 +0530

    Updates

---
+++ precache-manifest.d5b94eff0f0e9bc2045e032860f46825.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d4fdc19a0de9ab428d2f8b95bf4f43c",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "abc5c62329993db76093",
+    "url": "/covid19/static/css/2.8eba8d72.chunk.css"
+  },
+  {
+    "revision": "825bcdd76762f0427fe9",
+    "url": "/covid19/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "abc5c62329993db76093",
+    "url": "/covid19/static/js/2.4b96fdd3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/covid19/static/js/2.4b96fdd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "825bcdd76762f0427fe9",
+    "url": "/covid19/static/js/main.7087a650.chunk.js"
+  },
+  {
+    "revision": "13050c66b8110e64ca78",
+    "url": "/covid19/static/js/runtime-main.61b1d6d4.js"
+  }
+]);
