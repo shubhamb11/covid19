286572b9f313eb332a0039dbbb198502bab8675d
Shubham Bhardwaj <Shubham@chalopadho.com>
2020-05-13 04:19:03 +0530

    header changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './App.css';
 import CustomTabs from './Components/CustomTabs';
+import { Helmet } from 'react-helmet';
 
-function App() {
-  return (
-    <div className="App">
-      <CustomTabs />  
-    </div>
-  );
+class App extends React.Component{
+  render(){
+    return (
+      <div className="App">
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>Covid19 Tracker</title>
+        </Helmet>
+        <CustomTabs />  
+      </div>
+    );
+  }
 }
 
 export default App;
